288f37fadac21a5edb27171fcdad29efa7ddf54e
docencia <docencia@U033312.vp.ehu.es>
2022-01-25 16:42:00 +0100

    Se añade nuevo metodo que manda nuevo mensaje

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -3,4 +3,8 @@ public class MiPrimeraClase {
 	public static void main(String[] args) {
 		System.out.println("Que tal");
 	}
+	public static void otroMetodo(){
+		System.out.println("Otro mensaje");
+
+	}
 }
